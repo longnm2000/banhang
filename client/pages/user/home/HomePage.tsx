2d54fe5219eee26eff99d8e16b0f6a96ab039bc2
longnm2000 <longnguyenmanh20052000@gmail.com>
2023-10-17 14:48:40 +0700

    bh

---
+++ client/pages/user/home/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import FooterComp from "../../../components/footer/FooterComp"
+import HeaderComp from "../../../components/header/HeaderComp"
+
+function HomePage() {
+    return (
+        <>
+            <HeaderComp />
+            <FooterComp />
+        </>
+    )
+}
+
+export default HomePage
---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from "../pages/user/login/LoginPage"
+import RegisterPage from "../pages/user/register/RegisterPage"
+import NotFoundPage from "../pages/NotFound/NotFoundPage"
+import HomePage from "../pages/user/home/HomePage"
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
